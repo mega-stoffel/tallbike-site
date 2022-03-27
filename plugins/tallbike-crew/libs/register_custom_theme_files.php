8ac98e3c5340efc03d4ac26faa5ff2a15e39d7e6
X-tof <x-tof.hoyer@posteo.de>
2022-03-27 23:28:37 +0200

    found the problem with the events!

--- plugins/tallbike-crew/libs/register_custom_theme_files.php
+++ plugins/tallbike-crew/libs/register_custom_theme_files.php
@@ -1,12 +1,12 @@
 <?php
-/**
+/*
  * Verlinkt die Plugin SINGLE Post-Type Seiten
  */
-function tallbike_plugin_custom_single_theme_file_include($template) {
-
+function tallbike_plugin_custom_single_theme_file_include($template)
+{
     global $wp;
-    
-    if ( isset($wp->query_vars['post_type']) ) // && $wp->query_vars['post_type'] == 'bikes' )
+   
+    if(isset($wp->query_vars['post_type'])) // && $wp->query_vars['post_type'] == 'bikes' )
     {
         $requested_post_type = $wp->query_vars['post_type'];
         
@@ -19,14 +19,12 @@ function tallbike_plugin_custom_single_theme_file_include($template) {
         }
         return $template;
     }
-    
     return $template;
-
 }
 
 add_filter('single_template', 'tallbike_plugin_custom_single_theme_file_include');
 
-/**
+/*
  * Verlinkt die Plugin ARCHIVE Post-Type Seiten
  */
 function tallbike_plugin_custom_archive_theme_file_include($template) {
@@ -34,17 +32,13 @@ function tallbike_plugin_custom_archive_theme_file_include($template) {
     global $wp;
     $requested_post_type = $wp->query_vars["post_type"];
 
-    if($requested_post_type == 'bikes')
+    if(($requested_post_type == 'bikes') || ($requested_post_type == 'events'))
     {
-        //return $template;
-
         $file = TALLBIKE_PLUGIN_PATH.'/theme_files/archive-'.$requested_post_type.'.php';
         if(file_exists($file)) {
             $template = $file;
         }
     }
-
     return $template;
-
 }
 add_action('archive_template', 'tallbike_plugin_custom_archive_theme_file_include');
