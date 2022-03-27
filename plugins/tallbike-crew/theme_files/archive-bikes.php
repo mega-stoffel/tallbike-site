9c23198c364954b106c8048b9715fa504d762b6a
X-tof <x-tof.hoyer@posteo.de>
2022-03-28 01:07:53 +0200

    first archive events

--- plugins/tallbike-crew/theme_files/archive-bikes.php
+++ plugins/tallbike-crew/theme_files/archive-bikes.php
@@ -1,19 +1,34 @@
 <?php
 
 /*
- * Template Name: Archive Bikes Template
+ * Template File: Archive for the Custom Post Type bikes
  *
- * Description: Eine eigene Archivseite für meinen Post Type "bikes"
+ * Description: Show a list of all our bikes
  * 
  */
 
 get_header();
+?>
+
+<div id="content" class="site-content">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            
+            
+<header class="entry-header alignwide">
+    <h1 class="entry-title">R&auml;der</h1>
+</header>
+            
+<div class="entry-content">
+
+<?php
 
 wp_reset_postdata();
 
 $queryArgs = array( 
     'post_type'	=> 'bikes',
-    'posts_per_page' => 10,
+    'posts_per_page' => 15,
     'orderby'   => 'rand',
     //'orderby'   => 'ID',
     //'order'     => 'ASC',
@@ -28,6 +43,7 @@ if ( $tb_bikes_query->have_posts() ) {
     echo '<ul>';
     while ( $tb_bikes_query->have_posts() ) {
         $tb_bikes_query->the_post();
+        $current_eventID = get_the_ID();
         echo '<li><img src="' . get_the_post_thumbnail_url(get_the_ID() ,'medium') .'">';
         echo ' <a href="'. get_the_permalink() .'">' . get_the_title() . '</a></li><br>';
     }
@@ -38,9 +54,6 @@ if ( $tb_bikes_query->have_posts() ) {
 /* Restore original Post Data */
 wp_reset_postdata();
 
-// ^^ this works!
-
-// the next chapter kind of works as well, but outputs just one bike....
 ?>
 
 <?php get_footer();
--- plugins/tallbike-crew/theme_files/archive-events.php
+++ plugins/tallbike-crew/theme_files/archive-events.php
@@ -1,60 +1,153 @@
 <?php
 
 /*
- * Template Name: Archive Events Template
- *
- * Description: Eine eigene Archivseite für meinen Post Type "events"
- * 
- */
+* Template File: Archive for the Custom Post Type events
+*
+* Description: Show a list of all our events
+* 
+*/
  
 get_header();
 
+?>
+
+<div id="content" class="site-content">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            
+            
+<header class="entry-header alignwide">
+    <h1 class="entry-title">Touren</h1>
+</header>
+            
+<div class="entry-content">
+                
+<?php
+
 wp_reset_postdata();
 
-$today = getdate();
+$tbtoday = date('Y-m-d');
 
-$queryArgs = array(
+// all previous events:
+$queryArgs_past = array(
 	'post_type'	=> 'events',
+    'order'     => 'DESC',
     'meta_key'  => 'events_cf_Date',
-    'before'    => 'January 1st, 2022',
     'orderby'   => 'events_cf_Date',
-    'order'     => 'ASC',
+    'meta_value'    => $tbtoday,
+    'meta_compare'  => '<=',
 );
 
-$the_query = new WP_Query($queryArgs);
+$queryArgs_future = array(
+	'post_type'	=> 'events',
+    'order'     => 'ASC',
+    'meta_key'  => 'events_cf_Date',
+    'orderby'   => 'events_cf_Date',
+    'meta_value'    => $tbtoday,
+    'meta_compare'  => '>=',
+);
+$tb_events_query = new WP_Query($queryArgs_future);
 
+echo "<p><h2>kommende Touren</h2>";
 // The Loop
-if ( $the_query->have_posts() ) {
+if ( $tb_events_query->have_posts() ) {
     echo '<ul>';
-    while ( $the_query->have_posts() ) {
-        $the_query->the_post();
-        echo '<li>' . get_the_title() . '</li>';
+    echo "\r\n";
+    while ( $tb_events_query->have_posts() ) {
+        $tb_events_query->the_post();
+        $current_eventID = get_the_ID();
+        $current_eventtimestamp = esc_attr( get_post_meta($current_eventID, 'events_cf_Date', true ) );
+        $eventDateFormatted = strtotime($current_eventtimestamp);
+        $current_eventDate = date("d.m.Y",$eventDateFormatted);
+        $current_eventTime = date("H:i",$eventDateFormatted);
+        //echo '<li>';
+        echo ' <a href="'. get_the_permalink() .'">';
+        the_title( '<h3 class="entry-title">', '</h3>');
+        echo '</a>';
+        echo "am " . $current_eventDate . " um " . $current_eventTime ."<br>";
+        echo "Treffpunkt: " . get_post_meta($current_eventID, 'events_cf_Place', true ) .'<br>';
+        echo "\r\n";
     }
     echo '</ul>';
 } else {
     // no posts found
 }
+echo "</p>";
+echo "<p><h2>gefahrene Touren</h2>";
+//echo "<p>";
+$tb_events_query = new WP_Query($queryArgs_past);
+
+// The Loop
+if ( $tb_events_query->have_posts() ) {
+    echo '<ul>';
+    echo "\r\n";
+    while ( $tb_events_query->have_posts() ) {
+        $tb_events_query->the_post();
+        $current_eventID = get_the_ID();
+        //echo '<li>';
+        echo ' <a href="'. get_the_permalink() .'">';
+        the_title( '<h3 class="entry-title">', '</h3>');
+        echo '</a>';
+        echo "Treffpunkt: " . get_post_meta($current_eventID, 'events_cf_Place', true ) .'<br>';
+        $current_eventtimestamp = esc_attr( get_post_meta($current_eventID, 'events_cf_Date', true ) );
+        $eventDateFormatted = strtotime($current_eventtimestamp);
+        $current_eventDate = date("d.m.Y",$eventDateFormatted);
+        $current_eventTime = date("H:i",$eventDateFormatted);
+        echo "am: " . $current_eventDate;
+        echo '<br>';
+        //echo ' <a href="'. get_the_permalink() .'">' . get_the_title() . '</a></li><br>';
+        echo "\r\n";
+    }
+    echo '</ul><br>';
+} else {
+    // no posts found
+}
+echo "</p>";
+
+// if ($the_query->have_posts())
+// {
+//     while ( have_posts() )
+//     {
+//         echo '<div class="row">';
+//         //echo 'content';
+//         the_title( '<h3 class="entry-title">', '</h3>' );
+//         echo '</div>';
+//     }
+//         //  while ( $the_query->have_posts() ) {
+        //      $the_query->the_post();
+        //      echo '<li>' . get_the_title() . '</li>';
+        //  }
+
+// 	echo '<header class="entry-header alignwide">';
+// 		//twenty_twenty_one_post_thumbnail();
+// 	echo '</header>'; //<!-- .entry-header -->
+
+//     //echo '<ul>';
+//     //echo '</ul>';
+// } else {
+//     // no posts found
+// }
+//echo '</p>';
 /* Restore original Post Data */
-wp_reset_postdata();
+//wp_reset_postdata();
 
 //'meta_key' => 'ecents_cf_Place',
 //'meta_value' => 'Marienplatz',
-?>
-some output!
-    <div class="main-wrap" role="main">
-        <!-- Darstellung der Events -->
-        <section id="events-listing">
-            <?php if ( $the_query->have_posts() ) : ?>
-                <div class="row">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <div class="column">
-	       <?php require_once( 'events-content.php' ); ?>
-	    </div>
-                <? endwhile; ?>
-                </div>
-            <?php endif; ?>
-        </section>
+    // <div class="main-wrap" role="main">
+    //     <!-- Darstellung der Events -->
+    //     <section id="events-listing">
+    //         <?php if ( $the_query->have_posts() ) : 
+    //             <div class="row">
+    //             <?php while ( have_posts() ) : the_post(); 
+    //                 <div class="column">
+	//        <?php require_once( 'events-content.php' ); 
+	//     </div>
+    //             <? endwhile; 
+    //             </div>
+    //         <?php endif; 
+    //     </section>
 
-    </div>
+    // </div>
 
-<?php get_footer();
+get_footer();
